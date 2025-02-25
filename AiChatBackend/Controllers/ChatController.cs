0447d9129f7c4cafbbeb2a089d6a005a0f1d97d0
Nazif Abdullah <ahmadnazif@gmail.com>
2025-02-25 18:59:14 +0800

    Completes references

--- AiChatBackend/Controllers/ChatController.cs
+++ AiChatBackend/Controllers/ChatController.cs
@@ -1,9 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.AI;
 
 namespace AiChatBackend.Controllers;
 
 [Route("api/chat")]
-public class ChatController : ControllerBase
+public class ChatController(IChatClient chatClient) : ControllerBase
 {
+    private readonly IChatClient chatClient = chatClient;
 
+    [HttpGet("send")]
+    public async Task<ActionResult<string>> Send([FromQuery] string? prompt, CancellationToken ct)
+    {
+        try
+        {
+            List<ChatMessage> msg = [];
+            msg.Add(new ChatMessage(ChatRole.User, prompt));
+
+            var resp = await chatClient.GetResponseAsync(msg, cancellationToken: ct);
+            return resp.Message.Text;
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }        
+    }
 }
--- AiChatBackend/Program.cs
+++ AiChatBackend/Program.cs
@@ -1,8 +1,26 @@
+using Microsoft.Extensions.AI;
+
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+
+builder.Services.AddChatClient(x =>
+{
+    const string ollama = "Ollama";
+    var endpoint = config[$"{ollama}:Endpoint"];
+    var model = config[$"{ollama}:Model"];
+
+    return new OllamaChatClient(endpoint, "llama3");
+});
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.WebHost.ConfigureKestrel(x =>
+{
+    var port = int.Parse(config["Port"]);
+    x.ListenAnyIP(port);
+});
 
 var app = builder.Build();
 
