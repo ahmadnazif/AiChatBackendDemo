fbceb017cf36e5cb8b9c6428b5d22fa08d471ab7
Nazif Abdullah <ahmadnazif@gmail.com>
2025-02-26 14:20:24 +0800

    Add ModelId property to ChatHubChatResponse

--- AiChatBackend/Models/ChatHubChatResponse.cs
+++ AiChatBackend/Models/ChatHubChatResponse.cs
@@ -5,4 +5,5 @@ public class ChatHubChatResponse
     public string? RequestMessage { get; set; }
     public string? ResponseMessage { get; set; }
     public TimeSpan Duration { get; set; }
+    public string? ModelId { get; set; }
 }
