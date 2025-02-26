810199115a97f85f1c503c70d57e6a92e45c7307
Nazif Abdullah <ahmadnazif@gmail.com>
2025-02-26 22:16:35 +0800

    Move ChatRequest record outside

--- AiChatBackend/Models/ChatHubBatchChatRequest.cs
+++ AiChatBackend/Models/ChatHubBatchChatRequest.cs
@@ -5,5 +5,3 @@ public class ChatHubBatchChatRequest
     public List<ChatRequest> Messages { get; set; }
 
 }
-
-public record ChatRequest(ChatSender Type, string Message);
---
+++ AiChatBackend/Models/ChatRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace AiChatBackend.Models;
+
+public record ChatRequest(ChatSender Type, string Message);
+
