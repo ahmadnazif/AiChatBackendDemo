68a1fd8c22593671a39a1fe82fce8da439d6c824
Nazif Abdullah <ahmadnazif@gmail.com>
2025-02-26 15:24:16 +0800

    Add HubController

---
+++ AiChatBackend/Controllers/HubController.cs
@@ -0,0 +1,29 @@
+﻿using AiChatBackend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AiChatBackend.Controllers;
+
+[Route("rest-api/hub")]
+[ApiController]
+public class HubController(IHubUserService user) : ControllerBase
+{
+    private readonly IHubUserService user = user;
+
+    [HttpGet("user/is-username-registered")]
+    public ActionResult<bool> IsUsernameRegistered([FromQuery] string? username) => user.IsExist(username, UserSessionKeyType.Username);
+
+    [HttpGet("user/is-connection-id-active")]
+    public ActionResult<bool> IsConnectionIdActive([FromQuery] string? connectionId) => user.IsActive(connectionId);
+
+    [HttpGet("user/get-by-username")]
+    public ActionResult<UserSession> GetUserSessionByUsername([FromQuery] string? username) => user.Find(username, UserSessionKeyType.Username);
+
+    [HttpGet("user/get-by-connection-id")]
+    public ActionResult<UserSession> GetUserSessionByConnectionId([FromQuery] string? connectionId) => user.Find(connectionId, UserSessionKeyType.ConnectionId);
+
+    [HttpGet("user/count-all")]
+    public ActionResult<int> CountAllUser() => user.CountAll();
+
+    [HttpGet("user/list-all")]
+    public ActionResult<List<UserSession>> ListAllUser() => user.ListAllActive();
+}
