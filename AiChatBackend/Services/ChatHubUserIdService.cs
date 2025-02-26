106d8c24646e586ae9b4ab39abed0e0362f9d8c0
Nazif Abdullah <ahmadnazif@gmail.com>
2025-02-26 13:20:53 +0800

    Add ChatHubUserIdService

---
+++ AiChatBackend/Services/ChatHubUserIdService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AiChatBackend.Services;
+
+public class ChatHubUserIdService : IUserIdProvider
+{
+    public string? GetUserId(HubConnectionContext connection)
+    {
+        return connection.GetHttpContext().Request.Query["username"];
+    }
+}
