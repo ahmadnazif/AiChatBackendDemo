b96005781559344e97ec13c36d16848fea0a26b1
Nazif Abdullah <ahmadnazif@gmail.com>
2025-05-14 22:35:23 +0800

    Improve the DB

--- AiChatBackend/Services/InMemoryVectorDb.cs
+++ AiChatBackend/Services/InMemoryVectorDb.cs
@@ -12,37 +12,36 @@ public class InMemoryVectorDb(ILogger<InMemoryVectorDb> logger, OllamaEmbeddingG
     private readonly InMemoryVectorStore store = store;
     private const string COLL_TEXT = "text";
 
-    //public async Task<ResponseBase> UpsertFoodAsync(FoodVectorModelBase food, CancellationToken ct)
-    //{
-    //    try
-    //    {
-    //        var coll = store.GetCollection<Guid, FoodVectorModel>(COLL_FOOD);
-    //        await coll.CreateCollectionIfNotExistsAsync(ct);
+    public async Task<ResponseBase> UpsertTextAsync(TextVectorBase data, CancellationToken ct)
+    {
+        try
+        {
+            var coll = store.GetCollection<Guid, TextVectorBase>(COLL_TEXT);
+            await coll.CreateCollectionIfNotExistsAsync(ct);
 
-    //        var id = await coll.UpsertAsync(new FoodVectorModel
-    //        {
-    //            Id = Guid.NewGuid(),
-    //            FoodName = food.FoodName,
-    //            Remarks = food.Remarks,
-    //            Vector = await gen.GenerateVectorAsync(food.Remarks)
-    //        }, ct);
+            var id = await coll.UpsertAsync(new TextVector
+            {
+                Id = Guid.NewGuid(),
+                Text = data.Text,
+                Vector = await gen.GenerateVectorAsync(data.Text)
+            }, ct);
 
-    //        return new()
-    //        {
-    //            IsSuccess = true,
-    //            Message = id.ToString()
-    //        };
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        logger.LogError(ex.Message);
-    //        return new()
-    //        {
-    //            IsSuccess = false,
-    //            Message = ex.Message
-    //        };
-    //    }
-    //}
+            return new()
+            {
+                IsSuccess = true,
+                Message = id.ToString()
+            };
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex.Message);
+            return new()
+            {
+                IsSuccess = false,
+                Message = ex.Message
+            };
+        }
+    }
 
 
     //public async Task<ResponseBase> UpsertFoodsAsync(List<FoodVectorModelBase> foods, CancellationToken ct)
