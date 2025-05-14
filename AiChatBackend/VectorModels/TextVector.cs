ff4d49ce39dfd8347747e54cc716c4fba1c10371
Nazif Abdullah <ahmadnazif@gmail.com>
2025-05-14 22:32:34 +0800

    Improve TextVectorBase

--- AiChatBackend/VectorModels/TextVector.cs
+++ AiChatBackend/VectorModels/TextVector.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.Extensions.VectorData;
 namespace AiChatBackend.VectorModels;
 
-public class TextVector
+public class TextVector : TextVectorBase
 {
     [VectorStoreRecordKey]
-    public ulong Id { get; set; }
+    public Guid Id { get; set; }
+    [VectorStoreRecordVector(348, DistanceFunction = DistanceFunction.CosineSimilarity, IndexKind = IndexKind.Hnsw)] public ReadOnlyMemory<float> Vector { get; set; }
+}
 
-    //[VectorStoreRecordData(IsIndexed = true, StoragePropertyName = "text_label")]
-    [VectorStoreRecordData] public string Label { get; set; }
-
-    //[VectorStoreRecordData(IsFullTextIndexed = true, StoragePropertyName = "text_remarks")]
-    [VectorStoreRecordData]    public string Remarks { get; set; }
-
-    //[VectorStoreRecordVector(4, DistanceFunction = DistanceFunction.CosineSimilarity, IndexKind = IndexKind.Hnsw, StoragePropertyName = "text_vector")]
-    [VectorStoreRecordVector(384, DistanceFunction = DistanceFunction.CosineDistance)]
-    public ReadOnlyMemory<float> Vector { get; set; }
+public class TextVectorBase
+{
+    [VectorStoreRecordData] public string Text { get; set; }
 }
