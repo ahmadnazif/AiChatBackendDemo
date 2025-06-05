b36f15328b991f49f3406a66e582e2247f084baa
Nazif Abdullah <ahmadnazif@gmail.com>
2025-06-05 17:37:30 +0800

    Add model TextSimilarityLlmRequest

---
+++ AiChatBackend/Models/TextSimilarityLlmRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace AiChatBackend.Models;
+
+public class TextSimilarityLlmRequest
+{
+    public string? OriginalPrompt { get; set; }
+    public List<string> Results { get; set; }
+    public string? ModelId { get; set; }
+}
